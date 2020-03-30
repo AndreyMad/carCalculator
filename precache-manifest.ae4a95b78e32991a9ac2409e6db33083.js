9ceea523b1cbadeb0f822dd932e6ed9c00f0aa8a
AndreyMad <andrey.mad.87@gmail.com>
2020-03-31 00:31:53 +0300

    Updates

---
+++ precache-manifest.ae4a95b78e32991a9ac2409e6db33083.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc0246167b360afae59242779413748",
+    "url": "/carsCalc/index.html"
+  },
+  {
+    "revision": "24b4bab5fbf66029f40b",
+    "url": "/carsCalc/static/css/main.19f0f34e.chunk.css"
+  },
+  {
+    "revision": "6460163b777adb470e88",
+    "url": "/carsCalc/static/js/2.d27d11c1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/carsCalc/static/js/2.d27d11c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24b4bab5fbf66029f40b",
+    "url": "/carsCalc/static/js/main.58def2dc.chunk.js"
+  },
+  {
+    "revision": "5e9e463f040bf9375b31",
+    "url": "/carsCalc/static/js/runtime-main.fdbe56fb.js"
+  },
+  {
+    "revision": "4acaa253b5177e8951210528f38ee332",
+    "url": "/carsCalc/static/media/logo.4acaa253b517.4acaa253.png"
+  }
+]);
