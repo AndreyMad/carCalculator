9cdcd4e645c4535ed93b50e91cb0d59a5ddcdaf4
AndreyMad <andrey.mad.87@gmail.com>
2020-04-11 18:24:05 +0300

    add portDep

--- src/pages/CalculatorPage/CalculatorPage.jsx
+++ src/pages/CalculatorPage/CalculatorPage.jsx
@@ -1,46 +1,76 @@
 import React, { Component } from "react";
 import Select from "react-select";
-import ports from "../../assets/data/ports.json";
+import places from "../../assets/data/places.json";
 import style from "./CalculatorPage.module.css";
-import * as maps from "../../assets/img/map/index";
+import ports from "../../assets/data/portDeparture.json";
 
 class CalculatorPage extends Component {
   state = {
     arrayOfDepartures: [],
+    departurePlaceForSelect: "",
     departurePlace: "",
     arrayOfPorts: [],
-    departurePort: ""
+    departurePorts: []
   };
 
   componentDidMount() {
     this.departureParse();
-    console.log(maps);
   }
 
   departureParse = () => {
-    const { Departures } = { ...ports };
+    // собираем список доступных локаций для react select
+    const { Departures } = { ...places };
     const departuresArray = [];
     Departures.map(el => {
       const valToArr = {
         value: el.city,
         label: el.city
-        //   state: el[1]
       };
       return departuresArray.push(valToArr);
     });
     this.setState({ arrayOfDepartures: departuresArray });
   };
 
-  search;
+  departurePortHandler = () => {
+    this.setState({ arrayOfPorts: [] });
+    const { departurePlaceForSelect } = this.state;
+    const { Departures } = { ...places };
+    const departPort = Departures.find(el => {
+      return el.city === departurePlaceForSelect;
+    });
+
+    const entries = Object.entries(departPort);
+    const { portsDepart } = { ...ports };
+    entries.map(el => {
+      if (el[0].length === 2 && el[1] !== null) {
+        const portToState = portsDepart.filter(port => {
+          return port.value === el[0];
+        });
+
+        this.setState(prevState => ({
+          arrayOfPorts: [...prevState.arrayOfPorts, el],
+          departurePorts: [...portToState]
+        }));
+      }
+      return null;
+    });
+  };
 
   imgStateHelper = () => {};
 
   handleChange = e => {
-    this.setState({ departurePlace: e.value });
+    this.setState(
+      {
+        departurePlaceForSelect: e.value
+      },
+      () => {
+        this.departurePortHandler();
+      }
+    );
   };
 
   render() {
-    const { arrayOfDepartures } = this.state;
+    const { arrayOfDepartures, departurePorts } = this.state;
     return (
       <>
         <div className={style.deliveryContainer}>
@@ -49,6 +79,16 @@ class CalculatorPage extends Component {
             <br />
             <Select options={arrayOfDepartures} onChange={this.handleChange} />
           </div>
+          {departurePorts.length > 0 ? (
+            <p className={style.departurePort}>
+              Порт відправки: {departurePorts[0].name}
+            </p>
+          ) : null}
+          {/* <div className={style.departWrapper}>
+            <p className={style.span}>Оберіть місцезнаходження автомобіля </p>
+            <br />
+            <Select options={arrayOfDepartures} onChange={this.handleChange} />
+          </div> */}
           {/* <img src=/> */}
         </div>
       </>
