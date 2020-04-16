fb86163135757362f890739f7e892a9926957c7f
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2020-04-16 17:50:16 +0300

    add modal styles

--- src/components/CallBack/CallBackModal.jsx
+++ src/components/CallBack/CallBackModal.jsx
@@ -7,11 +7,17 @@ class CallBackModal extends Component {
     formValues: ""
   };
 
-  componentDidMount() {}
-
   static propTypes = {
     toggleModal: PropTypes.func.isRequired
   };
+
+  componentDidMount() {
+    document.body.style = " overflow: hidden ";
+  }
+
+  componentWillUnmount() {
+    document.body.style = "";
+  }
 
   formSubmit = e => {
     e.preventDefault();
@@ -25,39 +31,48 @@ class CallBackModal extends Component {
     return (
       <>
         <div className={style.overlay}>
-          <h3 className={style.title}>
-            Залиште Ваші контактні дані і наш менеджер звяжеться з вами протягом
-            30хв
-          </h3>
           <div className={style.container}>
-            <form onSubmit={this.formSubmit}>
-              <label htmlFor="phoneNumber">
-                Номер телефону:
-                <input type="number" className={style.input} id="pnoneNumber" />
-              </label>
-              <label htmlFor="nameInput">
-                Ваше імя:
-                <input
-                  type="text"
-                  value={formValues}
-                  className={style.input}
-                  id="nameInput"
-                />
-              </label>
-              <label htmlFor="comment">
-                Коментар:
-                <textarea className={style.textArea} id="comment" />
-              </label>
-            </form>
-            <button
-              type="button"
-              onClick={toggleModal}
-              className={style.closeButton}
-            />
-            <button
-              className={style.CallBackButton}
-              onClick={this.formSubmit}
-            />
+            <h3 className={style.title}>
+              Залиште Ваші контактні дані і наш менеджер звяжеться з вами
+              протягом 30хв
+            </h3>
+            <div className={style.formWrappe}>
+              <form onSubmit={this.formSubmit}>
+                <label htmlFor="phoneNumber">
+                  Номер телефону:
+                  <input
+                    type="number"
+                    className={style.input}
+                    id="pnoneNumber"
+                  />
+                </label>
+                <label htmlFor="nameInput">
+                  Ваше імя:
+                  <input
+                    type="text"
+                    value={formValues}
+                    className={style.input}
+                    id="nameInput"
+                  />
+                </label>
+                <label htmlFor="comment">
+                  Коментар:
+                  <textarea className={style.textArea} id="comment" />
+                </label>
+              </form>
+              <button
+                type="button"
+                onClick={toggleModal}
+                className={style.closeButton}
+              />
+              <button
+                className={style.CallBackSubmitButton}
+                onClick={this.formSubmit}
+                type="button"
+              >
+                Залишити заявку
+              </button>
+            </div>
           </div>
         </div>
       </>
