7816444733856f0f06bc41d2b4224aa037c6a7a9
AndreyMad <andrey.mad.87@gmail.com>
2020-05-05 00:20:02 +0300

    styles search page

--- src/components/ErrorNotif/ErrorNotif.js
+++ src/components/ErrorNotif/ErrorNotif.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import oopsImg from "../../assets/img/8885322-oops-icon.jpg";
+import style from "./ErrorNotif.module.css";
+import routes from "../../routes/routes";
 
 const ErrorNotif = () => {
   return (
-    <div>
-      <p>Лот отсутствует или не корректный номер лота</p>
-    </div>
+    <>
+      <div className={style.container}>
+        <img className={style.img} src={oopsImg} alt="oops " />
+        <h3 className={style.title}>Номер лоту не знайдено. </h3>
+        <p className={style.text}>
+          Щось пішло не так...
+          <p className={style.text}>
+            Перевірте номер лоту і спробуйте ще раз, або скористайтесь
+            <NavLink className={style.link} to={routes.CALCULATOR_PAGE.path}>
+              {" "}
+              калькулятором
+            </NavLink>
+          </p>
+        </p>
+      </div>
+    </>
   );
 };
 
--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -15,39 +15,39 @@ import style from "./SearchPage.module.css";
 
 class SearchPage extends Component {
   state = {
-    // car: {
-    //   currentBid: 0,
-    //   buyNow: 0,
-    //   lot: 26098920,
-    //   aucDate: 1587996000000,
-    //   vin: "2FMDK39C57B******",
-    //   name: "2007 FORD EDGE SEL PLUS",
-    //   year: 2007,
-    //   city: "SAVANNAH",
-    //   state: "GA",
-    //   seller: "State Farm Insurance",
-    //   fuel: "GAS",
-    //   engine: "3.5L  6",
-    //   highlights: "RUNS AND DRIVES",
-    //   odo: 183964,
-    //   capacity: 3.5,
-    //   images: [
-    //     "https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX236/6d16dcf5-cd4c-4bf2-ba6e-e2f7f383b1e6.JPG",
-    //     "https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX244/9db09217-40dc-4c08-84a4-77d61c06483a.JPG",
-    //     "https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX244/e83a2570-9914-4cc2-92fd-2ed5bca19c2d.JPG"
-    //   ],
-    //   doc: "GA - CERT OF TITLE-SALVAGE"
-    // },
-    // averagePriceCar: {
-    //   make: "",
-    //   model: "",
-    //   year: ""
-    // },
-    // averagePrice: "",
-    // isLoading: false,
-    // error: "",
-    // lotPrice: "",
-    // selectedAuction: ""
+    car: {
+      currentBid: 0,
+      buyNow: 0,
+      lot: 26098920,
+      aucDate: 1587996000000,
+      vin: "2FMDK39C57B******",
+      name: "2007 FORD EDGE SEL PLUS",
+      year: 2007,
+      city: "SAVANNAH",
+      state: "GA",
+      seller: "State Farm Insurance",
+      fuel: "GAS",
+      engine: "3.5L  6",
+      highlights: "RUNS AND DRIVES",
+      odo: 183964,
+      capacity: 3.5,
+      images: [
+        "https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX236/6d16dcf5-cd4c-4bf2-ba6e-e2f7f383b1e6.JPG",
+        "https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX244/9db09217-40dc-4c08-84a4-77d61c06483a.JPG",
+        "https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX244/e83a2570-9914-4cc2-92fd-2ed5bca19c2d.JPG"
+      ],
+      doc: "GA - CERT OF TITLE-SALVAGE"
+    },
+    averagePriceCar: {
+      make: "",
+      model: "",
+      year: ""
+    },
+    averagePrice: "",
+    isLoading: false,
+    error: "",
+    lotPrice: "",
+    selectedAuction: ""
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -122,7 +122,13 @@ class SearchPage extends Component {
   };
 
   formSubmit = (value, selectedAuction, lotPrice) => {
-    this.setState({ isLoading: true, lotPrice, selectedAuction });
+    this.setState({
+      isLoading: true,
+      lotPrice,
+      selectedAuction,
+      car: {},
+      error: ""
+    });
     API.getCarByLot(value, selectedAuction)
       .then(res => {
         if (res.err) {
@@ -151,11 +157,10 @@ class SearchPage extends Component {
           <SearchForm formSubmit={this.formSubmit} />
           {car ? (
             <>
-              {" "}
               {car.images ? (
                 <CarInfo car={car} averagePrice={averagePrice} />
               ) : null}
-              {error ? <ErrorNotif error={error} /> : null}
+
               {car.lot > 5 ? (
                 <SearchCalc
                   car={car}
@@ -165,8 +170,9 @@ class SearchPage extends Component {
               ) : null}
             </>
           ) : null}
+          {error ? <ErrorNotif error={error} /> : null}
 
-          <CallBackBtn />
+          {car && error.length === 0 ? <CallBackBtn /> : null}
           <Footer />
         </div>
       </>
