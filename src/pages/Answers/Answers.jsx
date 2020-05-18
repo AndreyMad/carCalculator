f35f79a27f20babba3b008c8e42414d9faa286a0
AndreyMad <andrey.mad.87@gmail.com>
2020-05-19 02:08:35 +0300

    add spoiler

--- src/pages/Answers/Answers.jsx
+++ src/pages/Answers/Answers.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import style from "./Answers.module.css";
 
 const Answers = () => {
-  return <div>ss</div>;
+  return (
+    <section className={style.container}>
+      <div className={style.wrapper}>
+        <ul>
+          <li className={style.spoiler}>
+            <input type="checkbox" />
+            <span className={style.question}>питання</span>
+
+            <div className={style.listInner}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+              explicabo iusto quidem eius ipsam a animi ea suscipit illo quo.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              pariatur sed quae aperiam fugiat minima. Aliquid ipsa inventore
+              mollitia nobis unde numquam voluptas, deleniti impedit debitis
+              excepturi asperiores animi quia placeat, alias autem repellat,
+              illum corporis recusandae quibusdam nemo at! Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Quisquam necessitatibus earum
+              nostrum nemo, iure eveniet placeat eligendi tempora fuga sed,
+              perspiciatis magnam aspernatur? Magnam reiciendis pariatur
+              incidunt corporis? Perspiciatis eum iusto est eveniet sunt
+              doloribus quasi ipsum maiores, dolorem deserunt? Est, corporis.
+              Provident eaque repudiandae voluptate cum itaque delectus nam
+              beatae fugiat veniam cumque, reprehenderit corporis quisquam,
+              velit magni, sint distinctio eius! Pariatur natus qui, sint omnis
+              corporis quo corrupti?
+            </div>
+          </li>
+
+          <li>5</li>
+          <li>4</li>
+          <li>3</li>
+          <li>2</li>
+          <li>1</li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Answers;
--- src/pages/PartsPage/PartsPage.js
+++ src/pages/PartsPage/PartsPage.js
@@ -1,13 +1,77 @@
 import React, { Component } from "react";
-import * as API from "../../api/api";
+import style from "./PartsPage.module.css";
 
 class PartsPage extends Component {
-  state = { imagePreviewUrl: null };
+  state = { name: "", phone: "", coment: "" };
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.id]: target.value });
+  };
+
+  submit = () => {
+    console.log("work");
+  };
 
   render() {
-    const { imagePreviewUrl } = this.state;
+    const { name, phone, coment } = this.state;
 
-    return <div>afsasdfasfd</div>;
+    return (
+      <>
+        <h2 className={style.title}>
+          Запчастини з Америки. Замовити автозапчастини з США
+        </h2>
+        <p>
+          Окрім безпосередньої покукпи авто з США, багато власників хотіли б
+          придбати автозапчастини для ремонту та обслуговування свого авто
+        </p>
+        <p>
+          Заповніть форму нижче і наші співробітники нададуть вам необхідну
+          інформацію щодо ремонту авто.
+        </p>
+        <form onSubmit={this.submit}>
+          <input
+            type="text"
+            value={name}
+            placeholder="Ваше імя"
+            id="name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Ваш телефон"
+            id="phone"
+            value={phone}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            id="coment"
+            value={coment}
+            placeholder="Коментар"
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Замовити" />
+        </form>
+        <p>
+          Ми співпрацюємо з постачальниками з Європи, США, Японії, Арабських
+          Еміратів та інших. Тому ви зможете бути впевенні що отримаєте тільки
+          якісні деталі за найнижчоюб ціною.
+        </p>
+        <ul>
+          <li>
+            Наявність на наших складах більш 100000 позиці, серед яких такі
+            групи деталей:
+          </li>
+          <li>кузовні елементи: крила, бампери, капоти, фари і т.д.</li>
+          <li>розхідні елементи: фільтра, мастила, ремені</li>
+          <li>елементи шассі: амортизатори, пружини, пневмобалониважелі</li>
+          <li>елементи рульового управління: рейки, насоси, вали, тяги</li>
+          <li>деталі гальмівної системи: диски, колодки, датчики</li>
+          <li>елементи сисеми запалювання: свічки, катушки, модулі</li>
+          <li>та багато іншого</li>
+        </ul>
+      </>
+    );
   }
 }
 
