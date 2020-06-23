e3965900552422945aa6b7f8a62c50f5cb685210
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2020-06-23 17:59:23 +0300

    pre pull

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,23 +1,46 @@
 /* eslint-disable camelcase */
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 import style from "./Loader.module.css";
 
-const Loader = () => {
-  const cssload_inner_one = `${style.cssload_inner} ${style.cssload_one}`;
-  const cssload_inner_two = `${style.cssload_inner} ${style.cssload_two}`;
-  const cssload_inner_three = `${style.cssload_inner} ${style.cssload_three}`;
+class Loader extends Component {
+  static propTypes = {
+    abortLoading: PropTypes.func.isRequired
+  };
 
-  return (
-    <>
-      <div className={style.overlay}>
-        <div className={style.cssload_loader}>
-          <div className={cssload_inner_one} />
-          <div className={cssload_inner_two} />
-          <div className={cssload_inner_three} />
+  componentDidMount() {
+    document.body.style = " overflow: hidden ";
+  }
+
+  componentWillUnmount() {
+    document.body.style = " ";
+  }
+
+  render() {
+    const { abortLoading } = this.props;
+    const cssload_inner_one = `${style.cssload_inner} ${style.cssload_one}`;
+    const cssload_inner_two = `${style.cssload_inner} ${style.cssload_two}`;
+    const cssload_inner_three = `${style.cssload_inner} ${style.cssload_three}`;
+    return (
+      <>
+        <div className={style.overlay}>
+          <div className={style.cssload_loader}>
+            <div className={cssload_inner_one} />
+            <div className={cssload_inner_two} />
+            <div className={cssload_inner_three} />
+          </div>
+          <button
+            type="button"
+            onClick={abortLoading}
+            className={style.abortButton}
+          >
+            Відмінити
+          </button>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default Loader;
