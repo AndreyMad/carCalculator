3465f464d7422b8c814363792e04b444f1dd6258
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2020-07-23 10:15:22 +0300

    git fix

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-
-const app = express();
-
-const port = process.env.port || 8000;
-
-app.use(express.static("build"));
-
-app.get("/*", (req, res) => {
-  res.send(__dirname, "build", "index.html");
-});
-
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
-=======
 const path = require("path");
 
 const app = express();
@@ -28,5 +13,4 @@ const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
   console.log(`Server starting on port ${port}`);
->>>>>>> newdesign
 });
